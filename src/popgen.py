6884fa3208ecd0e6ffac353a16f2eb38eed0612d
ryancoffee <coffeer76@gmail.com>
2018-07-22 20:09:36 -0700

    builds hard coded phenotype sets

--- src/popgen.py
+++ src/popgen.py
@@ -3,62 +3,120 @@
 import numpy as np
 import tensorflow as tf
 
-#def sample_population():
-
 class Phenotype:
 	ndims = 10;
-	phtype = 0;
+	phID = 0;
 
 	def __init__(self,n,p):
 		self.ndims = n 
-		self.phtype = p 
+		self.phID = p 
 		self.meanvec = np.zeros(n,dtype=float)
 
 	def setmeans(self):
-		if self.phtype != 0:
+		if self.phID != 0:
 			self.meanvec = np.ones(self.ndims,dtype=float)
 		else:
 			self.meanvec = np.zeros(self.ndims,dtype=float)
-		if self.phtype == 2:
+		if self.phID == 2:
 			self.meanvec[1] = 0.25
-			self.meanvec[3] = 1.25
+			self.meanvec[-1] = 1.25
 		return self
 
 	def setstds(self):
-		if self.phtype !=0:
-			self.stdsvec = float(2.5)*np.ones(self.ndims,dtype=float)
+		if self.phID !=0:
+			self.stdsvec = float(0.5)*np.ones(self.ndims,dtype=float)
 		else:
-			self.stdsvec = float(1.5)*np.ones(self.ndims,dtype=float)
+			self.stdsvec = float(5)*np.ones(self.ndims,dtype=float)
+		if self.phID == 2:
+			self.stdsvec[1] = 2.0
+			self.stdsvec[-1] = 1.0
 		return self
 
 	def getmeans(self):
 		return self.meanvec
 	def getstds(self):
 		return self.stdsvec
+	def getphID(self):
+		return self.phID
 
 
 	def printout(self):
 		print('means = ', self.meanvec)
 		print('stds = ', self.stdsvec)
+		print('phID = ', self.phID)
 		return self
 
+
+
+
+
 class Individual:
-	identity = int(0);
-	def __init__():
-		#self.phtype = Phenotype(5,1)
-		self.identity = int(0)
+	def __init__(self,ph):
+		self.phtype = ph 
+		self.phtype.setmeans().setstds()
+		#self.phtype.printout()
+		self.ndims = self.phtype.ndims
+
+	def samplepheno(self):
+		self.coords = np.random.normal(self.phtype.getmeans(),self.phtype.getstds());
 		return self
+	def getcoords(self):
+		return self.coords
+	def getphID(self):
+		return self.phtype.getphID()
+
+	def printout(self):
+		print(self.getcoords(),self.getphID())
+		return self
+	
+
+
+
+
+
+"""
+**********************
+**********************
+*******	MAIN *********
+"""
 
 
 def main():
-	NDIMS = int(5);
-	mytype = Phenotype(NDIMS,0)
+	NDIMS = int(4);
 
-	mytype.setmeans().setstds().printout()
+	nphenos = 3
 
+	"""
+	type0 = Phenotype(NDIMS,1)
+	type1 = Phenotype(NDIMS,1)
 	type2 = Phenotype(NDIMS,2)
-	
+	type0.setmeans().setstds().printout()
+	type1.setmeans().setstds().printout()
 	type2.setmeans().setstds().printout()
+
+	print("OK, now copmputing individuals from statistical distributions")
+
+	#individual = Individual(type2)
+	#individual.samplepheno().printout()
+	"""
+
+	setsz = 100
+	outvec = np.zeros((setsz*nphenos,NDIMS+1),dtype=float)
+	phenoset = np.empty(setsz,dtype=object)
+	for j in range(nphenos):
+		phtype = Phenotype(NDIMS,j)
+		phtype.setmeans().setstds()
+		for i in range(phenoset.shape[0]):
+			phenoset[i] = Individual(phtype)
+			phenoset[i].samplepheno()
+			outvec[j*setsz+i,:-1] = phenoset[i].getcoords()
+			outvec[j*setsz+i,-1] = phenoset[i].getphID()
+	
+
+	np.savetxt('./data/testing.dat',outvec,fmt='%.4f');
+
+
+
 
 
 
